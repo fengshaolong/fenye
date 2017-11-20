7aca7f185c7410818f0a7b1e5857cd934d9c1f39
songjiangwei <1213131411@qq.com>
2017-11-20 11:20:11 +0800

    init git files

---
+++ js/music.js
@@ -0,0 +1,17 @@
+// 音乐的js代码
+var music = {
+	changeClass: function (target,id) {
+       	var className = $(target).attr('class');
+       	var ids = document.getElementById(id);
+       	(className == 'on')
+           	? $(target).removeClass('on').addClass('off')
+           	: $(target).removeClass('off').addClass('on');
+       	(className == 'on')
+           	? ids.pause()
+           	: ids.play();
+   	},
+	play:function(){
+		document.getElementById('media').play();
+	}
+}
+music.play();
